67ad208b9e4f56d2c2c418a3f6ec2f99cd16a1eb
Mykyta Mykulskyi <81817136+JokeUrSelf@users.noreply.github.com>
2025-08-05 18:09:43 +0200

    refactor: add StorageItem

--- src/lib/definitions/StorageArray.svelte.ts
+++ src/lib/definitions/StorageArray.svelte.ts
@@ -1,7 +1,7 @@
 import { SvelteSet } from "svelte/reactivity";
 
 export class StorageItem<T extends object> {
-    readonly data: T;
+    private data: T;
     readonly key: string;
     readonly storage: Storage;
     private callbacks = new SvelteSet<(data: T) => void>();
@@ -18,62 +18,28 @@ export class StorageItem<T extends object> {
     };
 
     set(value: T) {
-        // this.data.;
+        this.data = value;
         this.storage.setItem(this.key, JSON.stringify(this.data));
         this.callbacks.forEach(callback => callback(value));
     };
-    
+
+    subscribe(fn: (data: T) => void) {
+        this.callbacks.add(fn);
+        const unsubscribe = () => this.callbacks.delete(fn);
+        return unsubscribe;
+    }
 }
 
-export class StorageArray<T> {
-    private data: T[];
-    readonly key: string;
-    readonly storage: Storage;
-    private callbacks = new SvelteSet<(data: T[]) => void>();
-
+export class StorageArray<T> extends StorageItem<T[]> {
     constructor(key: string, defaultValue: T[] = [], storage: Storage = localStorage) {
-        const _data = localStorage.getItem(key)
-        this.data = $state(_data ? JSON.parse(_data) : defaultValue);
-        this.key = key;
-        this.storage = storage;
-    }
-
-    get(): T[] {
-        return this.data;
-    };
-
-    set(value: T[]) {
-        this.data = value
-        this.storage.setItem(this.key, JSON.stringify(this.data));
-        this.callbacks.forEach(callback => callback(value));
-    };
-
-    merge(value: T[], mergeCallback: (a: T, b: T) => boolean = () => true) {
-        const data = this.get();
-        const mergedArr = [...value];
-
-        for (const x of data) {
-            for (const y of mergedArr) {
-                if (!mergeCallback(x, y)) {
-                    mergedArr.push(y);
-                }
-            }
-        }
-
-        this.set(mergedArr);
+        super(key, defaultValue, storage);
     }
 
     push(...args: T[]) {
-        this.set([...this.data, ...args])
+        this.set([...this.get(), ...args])
     }
 
     remove(value: T) {
-        this.set(this.data.filter(item => item !== value));
-    }
-
-    subscribe(callback: (data: T[]) => void) {
-        this.callbacks.add(callback);
-        const unsubscribe = () => this.callbacks.delete(callback);
-        return unsubscribe;
+        this.set(this.get().filter(item => item !== value));
     }
 }
